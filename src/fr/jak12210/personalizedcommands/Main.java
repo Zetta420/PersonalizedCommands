c7e07ba7af510801751149dbafa31339918d6fe2
jak12210 <jak12210.gmod@gmail.com>
2017-01-28 16:01:38 +0100

    Updates

---
+++ src/fr/jak12210/personalizedcommands/Main.java
@@ -0,0 +1,10 @@
+package fr.jak12210.personalizedcommands;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	public void onEnable(){
+		
+	}
+}
