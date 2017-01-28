4fb2f8603897e8f1eb590d64dc161abcfdf906ca
jak12210 <jak12210.gmod@gmail.com>
2017-01-28 21:46:16 +0100

    Updates

--- src/fr/jak12210/personalizedcommands/Commands.java
+++ src/fr/jak12210/personalizedcommands/Commands.java
@@ -19,20 +19,89 @@ public class Commands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
 		if(sender instanceof Player){
 			Player p = (Player)sender;
+			String uuid = p.getUniqueId().toString();
+			String language = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
 			if(cmd.getName().equalsIgnoreCase("staff")){
 				if(args.length == 0){
-					String uuid = p.getUniqueId().toString();
-			        String language = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
 			        if(language.equalsIgnoreCase("FR")) {
-					p.sendMessage(plugin.getConfig().getString("fr.staff.title"));
+					p.sendMessage(plugin.getConfig().getString("fr.staff.title").replace("&", "§"));
+					p.sendMessage(plugin.getConfig().getString("fr.staff.content").replace("&", "§"));
 			        }else{
-			        p.sendMessage(plugin.getConfig().getString("en.staff.title"));
+			        p.sendMessage(plugin.getConfig().getString("en.staff.title").replace("&", "§"));
+			        p.sendMessage(plugin.getConfig().getString("en.staff.content").replace("&", "§"));
 			        }
+					return true;
+				}
+			}
+			if(cmd.getName().equalsIgnoreCase("english")){
+				if(args.length == 0){
+			        AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "en");
+					p.sendMessage(plugin.getConfig().getString("en.lang-change").replace("&", "§"));
+					return true;
+				}
+			}
+			if(cmd.getName().equalsIgnoreCase("francais")){
+				if(args.length == 0){
+			        AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "fr");
+					p.sendMessage(plugin.getConfig().getString("fr.lang-change").replace("&", "§"));
+					return true;
+				}
+			}
+			if(cmd.getName().equalsIgnoreCase("help") || cmd.getName().equalsIgnoreCase("aide")){
+				if(args.length == 0){
+					if(language.equalsIgnoreCase("FR")) {
+					p.sendMessage(plugin.getConfig().getString("fr.help.title").replace("&", "§"));
+					p.sendMessage(plugin.getConfig().getString("fr.help.page1").replace("&", "§"));
+					}else{
+					p.sendMessage(plugin.getConfig().getString("en.help.title").replace("&", "§"));
+					p.sendMessage(plugin.getConfig().getString("en.help.page1").replace("&", "§"));
+					}
+					return true;
+				}
+			}
+			if(cmd.getName().equalsIgnoreCase("plugins") || cmd.getName().equalsIgnoreCase("pl") || cmd.getName().equalsIgnoreCase("bukkit:pl")){
+				if(args.length == 0){
+					if(language.equalsIgnoreCase("FR")) {
+					p.sendMessage(plugin.getConfig().getString("fr.plugins.title").replace("&", "§"));
+					p.sendMessage(plugin.getConfig().getString("fr.plugins.content").replace("&", "§"));
+					}else{
+					p.sendMessage(plugin.getConfig().getString("en.plugins.title").replace("&", "§"));
+					p.sendMessage(plugin.getConfig().getString("en.plugins.content").replace("&", "§"));
+					}
+					return true;
+				}
+			}
+			if(cmd.getName().equalsIgnoreCase("rules") || cmd.getName().equalsIgnoreCase("regles")){
+				if(args.length == 0){
+					if(language.equalsIgnoreCase("FR")) {
+					p.sendMessage(plugin.getConfig().getString("fr.rules.title").replace("&", "§"));
+					p.sendMessage(plugin.getConfig().getString("fr.rules.content").replace("&", "§"));
+					}else{
+					p.sendMessage(plugin.getConfig().getString("en.rules.title").replace("&", "§"));
+					p.sendMessage(plugin.getConfig().getString("en.rules.content").replace("&", "§"));
+					}
+					return true;
+				}
+			}
+			if(cmd.getName().equalsIgnoreCase("lang")){
+				if(args.length == 0){
+					String lang = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
+					if(lang.equalsIgnoreCase("fr")){
+						AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "en");
+						p.sendMessage(plugin.getConfig().getString("en.lang-change").replace("&", "§"));
+					}else{
+						AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "fr");
+						p.sendMessage(plugin.getConfig().getString("fr.lang-change").replace("&", "§"));
+					}
+			        
+					
 					return true;
 				}
 			}
 		}
 		return false;
 	}
+		
+	}
 
-}
+	
--- src/fr/jak12210/personalizedcommands/Main.java
+++ src/fr/jak12210/personalizedcommands/Main.java
@@ -11,7 +11,16 @@ public class Main extends JavaPlugin implements Listener {
 	public void onEnable(){
 		
 		System.out.println("PersonalizedCommands (jak12210) > INITIALIZED");
-		getCommand("staff").setExecutor(new Commands(null));
+		getCommand("staff").setExecutor(new Commands(this));
+		getCommand("english").setExecutor(new Commands(this));
+		getCommand("francais").setExecutor(new Commands(this));
+		getCommand("help").setExecutor(new Commands(this));
+		getCommand("aide").setExecutor(new Commands(this));
+		getCommand("plugins").setExecutor(new Commands(this));
+		getCommand("pl").setExecutor(new Commands(this));
+		getCommand("rules").setExecutor(new Commands(this));
+		getCommand("regles").setExecutor(new Commands(this));
+		getCommand("lang").setExecutor(new Commands(this));
 	}
 	public void onDisable(){
 		System.out.println("PersonalizedCommands (jak12210) > DESACTIVATED");
