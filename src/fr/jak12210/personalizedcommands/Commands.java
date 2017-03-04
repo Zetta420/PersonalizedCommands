6cff22bc21977c45ec4c007524311aec06188095
jak12210 <jak12210.gmod@gmail.com>
2017-03-04 16:56:33 +0100

    Update

--- src/fr/jak12210/personalizedcommands/Commands.java
+++ src/fr/jak12210/personalizedcommands/Commands.java
@@ -1,9 +1,14 @@
 package fr.jak12210.personalizedcommands;
 
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 
 
@@ -15,24 +20,43 @@ public class Commands implements CommandExecutor {
 	public Commands(Plugin plugin) {
 		this.plugin = plugin;
 	}
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
+	public boolean myTextCommand1(CommandSender sender, Command cmd, String msg, String[] args, String configfr, String configen, String commande){
 		if(sender instanceof Player){
 			Player p = (Player)sender;
 			String uuid = p.getUniqueId().toString();
 			String language = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
-			if(cmd.getName().equalsIgnoreCase("staff")){
+			if(cmd.getName().equalsIgnoreCase(commande)){
 				if(args.length == 0){
-			        if(language.equalsIgnoreCase("FR")) {
-					p.sendMessage(plugin.getConfig().getString("fr.staff.title").replace("&", "§"));
-					p.sendMessage(plugin.getConfig().getString("fr.staff.content").replace("&", "§"));
-			        }else{
-			        p.sendMessage(plugin.getConfig().getString("en.staff.title").replace("&", "§"));
-			        p.sendMessage(plugin.getConfig().getString("en.staff.content").replace("&", "§"));
-			        }
+					if(language.equalsIgnoreCase("FR")) {
+							while(configfr.length() >= 1){
+						p.sendMessage(plugin.getConfig().getStringList(configfr));
+							}
+						
+					}else{
+						while(configen.length() <= 1){
+						p.sendMessage(plugin.getConfig().getStringList(configen));
+						}
+					}
 					return true;
+					
 				}
 			}
+		}
+		return false;
+		
+	}
+	@Override
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
+		String configfr = "fr.staff.title";
+		String configen = "en.staff.title";
+		String commande = "staff";
+		myTextCommand1(sender, cmd, msg, args, configfr, configen, commande);
+		if(sender instanceof Player){
+			Player p = (Player)sender;
+			String uuid = p.getUniqueId().toString();
+			String language = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
+			
 			if(cmd.getName().equalsIgnoreCase("english")){
 				if(args.length == 0){
 			        AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "en");
@@ -59,7 +83,7 @@ public class Commands implements CommandExecutor {
 					return true;
 				}
 			}
-			if(cmd.getName().equalsIgnoreCase("plugins") || cmd.getName().equalsIgnoreCase("pl") || cmd.getName().equalsIgnoreCase("bukkit:pl")){
+			if(cmd.getName().equalsIgnoreCase("plugins") || cmd.getName().equalsIgnoreCase("pl") || cmd.getName().startsWith("bukkit:")){
 				if(args.length == 0){
 					if(language.equalsIgnoreCase("FR")) {
 					p.sendMessage(plugin.getConfig().getString("fr.plugins.title").replace("&", "§"));
@@ -85,8 +109,7 @@ public class Commands implements CommandExecutor {
 			}
 			if(cmd.getName().equalsIgnoreCase("lang")){
 				if(args.length == 0){
-					String lang = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
-					if(lang.equalsIgnoreCase("fr")){
+					if(language.equalsIgnoreCase("fr")){
 						AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "en");
 						p.sendMessage(plugin.getConfig().getString("en.lang-change").replace("&", "§"));
 					}else{
@@ -98,9 +121,25 @@ public class Commands implements CommandExecutor {
 					return true;
 				}
 			}
+			if(cmd.getName().equalsIgnoreCase("announce") || cmd.getName().equalsIgnoreCase("annonce")){
+				if(args.length <= 0){
+				p.getServer().broadcastMessage(plugin.getConfig().getString("broadcast.prefix").replace("&", "§") + args);
+				}
+			}
 		}
 		return false;
 	}
+	private void myTextCommand(CommandSender sender, Command cmd, String msg, String[] args, String configfr,
+			String configen, String commande) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void onPlayerJoin(PlayerJoinEvent event){
+        event.getPlayer().getWorld().strikeLightningEffect(event.getPlayer().getLocation());
+        for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+            p.sendMessage((ChatColor.DARK_RED + p.getName() + ChatColor.GOLD + " has joined the Game with Holy Powers!"));
+        }
+    }
 	
 	
 		
--- src/fr/jak12210/personalizedcommands/Main.java
+++ src/fr/jak12210/personalizedcommands/Main.java
@@ -1,7 +1,11 @@
 package fr.jak12210.personalizedcommands;
 
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -12,6 +16,7 @@ public class Main extends JavaPlugin implements Listener {
 	public void onEnable(){
 		
 		System.out.println("PersonalizedCommands (jak12210) > INITIALIZED");
+		PluginManager pm = getServer().getPluginManager();
 		getCommand("staff").setExecutor(new Commands(this));
 		getCommand("english").setExecutor(new Commands(this));
 		getCommand("francais").setExecutor(new Commands(this));
@@ -22,10 +27,16 @@ public class Main extends JavaPlugin implements Listener {
 		getCommand("rules").setExecutor(new Commands(this));
 		getCommand("regles").setExecutor(new Commands(this));
 		getCommand("lang").setExecutor(new Commands(this));
+		pm.registerEvents(new BloodEffects(this), this);
 	}
 	public void onDisable(){
 		System.out.println("PersonalizedCommands (jak12210) > DESACTIVATED");
 	}
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+	      Player p = e.getPlayer();
+	      p.sendMessage("Bienvenue sur le serveur !");
+	   }
 	
 	
 }
