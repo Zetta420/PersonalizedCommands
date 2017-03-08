b95f0ffb245a498f9e2759717fc2320aef630890
jak12210 <jak12210.gmod@gmail.com>
2017-03-08 22:14:16 +0100

    Updates

--- src/fr/jak12210/personalizedcommands/Commands.java
+++ src/fr/jak12210/personalizedcommands/Commands.java
@@ -20,119 +20,35 @@ public class Commands implements CommandExecutor {
 	public Commands(Plugin plugin) {
 		this.plugin = plugin;
 	}
-	public boolean myTextCommand1(CommandSender sender, Command cmd, String msg, String[] args, String configfr, String configen, String commande){
-		if(sender instanceof Player){
-			Player p = (Player)sender;
-			String uuid = p.getUniqueId().toString();
-			String language = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
-			if(cmd.getName().equalsIgnoreCase(commande)){
-				if(args.length == 0){
-					if(language.equalsIgnoreCase("FR")) {
-						for (String monMessage : plugin.getConfig().getStringList(configfr)) {
-						p.sendMessage(monMessage.replace("&", "§"));
-						}
-						
-					}else{
-						for (String monMessage : plugin.getConfig().getStringList(configfr)) {
-						p.sendMessage(monMessage.replace("&", "§"));
-						}
-					}
-					return true;
-					
-				}
-			}
-		}
-		return false;
-		
-	}
+	public void SendMessageLang(CommandSender sender, String messagefr, String messageen) {
+        Player p = (Player) sender;
+        String uuid = p.getUniqueId().toString();
+        String language = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
+
+        
+            language.equalsIgnoreCase("FR");
+            for (String monMessage : plugin.getConfig().getStringList(messagefr)) {
+                p.sendMessage(monMessage.replace("&", "§"));
+            }
+        
+            for (String monMessage : plugin.getConfig().getStringList(messageen)) {
+                p.sendMessage(monMessage.replace("&", "§"));
+            }
+        
+    }
 	@Override
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String msg, String[] args) {
-		String configfr = "fr.staff";
-		String configen = "en.staff.title";
-		String commande = "staff";
-		myTextCommand1(sender, cmd, msg, args, configfr, configen, commande);
-		if(sender instanceof Player){
-			Player p = (Player)sender;
-			String uuid = p.getUniqueId().toString();
-			String language = AdvancedMultiLanguageAPI.getLanguageOfUuid(uuid);
-			
-			if(cmd.getName().equalsIgnoreCase("english")){
-				if(args.length == 0){
-			        AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "en");
-					p.sendMessage(plugin.getConfig().getString("en.lang-change").replace("&", "§"));
-					return true;
-				}
-			}
-			if(cmd.getName().equalsIgnoreCase("francais")){
-				if(args.length == 0){
-			        AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "fr");
-					p.sendMessage(plugin.getConfig().getString("fr.lang-change").replace("&", "§"));
-					return true;
-				}
-			}
-			if(cmd.getName().equalsIgnoreCase("help") || cmd.getName().equalsIgnoreCase("aide")){
-				if(args.length == 0){
-					if(language.equalsIgnoreCase("FR")) {
-					p.sendMessage(plugin.getConfig().getString("fr.help.title").replace("&", "§"));
-					p.sendMessage(plugin.getConfig().getString("fr.help.page1").replace("&", "§"));
-					}else{
-					p.sendMessage(plugin.getConfig().getString("en.help.title").replace("&", "§"));
-					p.sendMessage(plugin.getConfig().getString("en.help.page1").replace("&", "§"));
-					}
-					return true;
-				}
-			}
-			if(cmd.getName().equalsIgnoreCase("plugins") || cmd.getName().equalsIgnoreCase("pl") || cmd.getName().startsWith("bukkit:")){
-				if(args.length == 0){
-					if(language.equalsIgnoreCase("FR")) {
-					p.sendMessage(plugin.getConfig().getString("fr.plugins.title").replace("&", "§"));
-					p.sendMessage(plugin.getConfig().getString("fr.plugins.content").replace("&", "§"));
-					}else{
-					p.sendMessage(plugin.getConfig().getString("en.plugins.title").replace("&", "§"));
-					p.sendMessage(plugin.getConfig().getString("en.plugins.content").replace("&", "§"));
-					}
-					return true;
-				}
-			}
-			if(cmd.getName().equalsIgnoreCase("rules") || cmd.getName().equalsIgnoreCase("regles")){
-				if(args.length == 0){
-					if(language.equalsIgnoreCase("FR")) {
-					p.sendMessage(plugin.getConfig().getString("fr.rules.title").replace("&", "§"));
-					p.sendMessage(plugin.getConfig().getString("fr.rules.content").replace("&", "§"));
-					}else{
-					p.sendMessage(plugin.getConfig().getString("en.rules.title").replace("&", "§"));
-					p.sendMessage(plugin.getConfig().getString("en.rules.content").replace("&", "§"));
-					}
-					return true;
-				}
-			}
-			if(cmd.getName().equalsIgnoreCase("lang")){
-				if(args.length == 0){
-					if(language.equalsIgnoreCase("fr")){
-						AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "en");
-						p.sendMessage(plugin.getConfig().getString("en.lang-change").replace("&", "§"));
-					}else{
-						AdvancedMultiLanguageAPI.setPlayerLanguage(uuid, "fr");
-						p.sendMessage(plugin.getConfig().getString("fr.lang-change").replace("&", "§"));
-					}
-			        
-					
-					return true;
-				}
-			}
-			if(cmd.getName().equalsIgnoreCase("announce") || cmd.getName().equalsIgnoreCase("annonce")){
-				if(args.length <= 0){
-				p.getServer().broadcastMessage(plugin.getConfig().getString("broadcast.prefix").replace("&", "§") + args);
-				}
+		;
+		for (String command : plugin.getConfig().getStringList("commandes")) {
+			if(cmd.getName().equalsIgnoreCase(command)){
+				String messagefr = "commandes." + command + ".fr";
+				String messageen = "commandes." + command + ".en";
+				SendMessageLang(sender, messagefr, messageen);
+				return true;
 			}
 		}
 		return false;
-	}
-	private void myTextCommand(CommandSender sender, Command cmd, String msg, String[] args, String configfr,
-			String configen, String commande) {
-		// TODO Auto-generated method stub
-		
 	}
 	public void onPlayerJoin(PlayerJoinEvent event){
         event.getPlayer().getWorld().strikeLightningEffect(event.getPlayer().getLocation());
